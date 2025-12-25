7d144a9ef79f7fcea6b177e475e9f551302a4dc3
meursault <linear23150@outlook.com>
2025-12-25 12:11:43 +0800

    add translate function

--- README.md
+++ README.md
@@ -4,20 +4,21 @@
  - ✅ **AI问答** - 接入了**chatgpt**
  - ✅ **简单计算** - 加法
  - ✅ **随机图片** - 第三方api
+ - ✅ **简单翻译** - tencent翻译api
  - ✅ **斜杠命令** - 支持支持 Hybrid Commands（同步后可在 Discord 直接输入 `/` 使用）
 
 ## 安装依赖
 > pip
 ```bash
-pip install openai discord.py requests
+pip install openai discord.py requests tencentcloud-sdk-python
 ```
 > uv
 ```bash
-uv add openai discord.py requests
+uv add openai discord.py requests tencentcloud-sdk-python
 ```
 
 ## 🚀快速开始
-1. 在 **.env** 文件中配置你的 **DISCORD_BOT_TOKEN** 和 **OPENAI_API_KEY**
+1. 在 **.env** 文件中配置你的 **DISCORD_BOT_TOKEN** ,**OPENAI_API_KEY** **Secretld**和**SecretKey**
 
 2. 运行脚本(推荐uv)
 >uv
--- main.py
+++ main.py
@@ -4,23 +4,26 @@ from discord.ext import commands
 import json
 import requests
 from openai import OpenAI
+from tencentcloud.common import credential
+from tencentcloud.tmt.v20180321 import tmt_client, models
 
 
+api_key = os.getenv("OPENAI_API_KEY")
 
-api_key=os.getenv('OPENAI_API_KEY')
-
-client=OpenAI(
-    api_key=api_key,
-    base_url="https://api.bltcy.ai/v1"
-)
+client = OpenAI(api_key=api_key, base_url="https://api.bltcy.ai/v1")
 
 
-token=os.getenv('DISCORD_BOT_TOKEN')
+token = os.getenv("DISCORD_BOT_TOKEN")
 
-intents=discord.Intents.default()
-intents.message_content= True
+secretld = os.getenv("Secretld")
+secretkey = os.getenv("SecretKey")
 
-bot =commands.Bot(command_prefix="!",intents=intents)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.command()
 @commands.has_permissions(administrator=True)
@@ -28,46 +31,62 @@ async def synccommands(ctx):
     await bot.tree.sync()
     await ctx.send("同步成功")
 
+
 @bot.hybrid_command()
 async def ping(ctx):
     await ctx.send("pong")
 
-@bot.hybrid_command()
-async def add(ctx,a:int,b:int):
-    await ctx.send(a+b)
 
 @bot.hybrid_command()
-async def ask(ctx,question:str):
+async def add(ctx, a: int, b: int):
+    await ctx.send(a + b)
+
+
+@bot.hybrid_command()
+async def ask(ctx, question: str):
     await ctx.defer()
-    response=  client.chat.completions.create(
-         model="gpt-4o",
-         messages=[
-             {"role":"user","content":question}
-         ]
+    response = client.chat.completions.create(
+        model="gpt-4o", messages=[{"role": "user", "content": question}]
     )
 
-    answer=response.choices[0].message.content
+    answer = response.choices[0].message.content
     await ctx.send(answer)
+
 
 @bot.hybrid_command()
 async def draw(ctx):
     await ctx.defer()
 
-    headers={
-         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
     }
 
-    url="https://www.98qy.com/sjbz/api.php?lx=dongman&format=json"
+    url = "https://www.98qy.com/sjbz/api.php?lx=dongman&format=json"
 
-    response=requests.get(url=url,headers=headers)
+    response = requests.get(url=url, headers=headers)
 
-    json_image=json.loads(response.content)
-    url_image=json_image['imgurl']
+    json_image = json.loads(response.content)
+    url_image = json_image["imgurl"]
     await ctx.send(url_image)
 
 
-bot.run(token)    
-    
+@bot.hybrid_command()
+async def translate(ctx, source: str, target: str, phrase: str):
+    await ctx.defer()
+    cred = credential.Credential(secret_id=secretld, secret_key=secretkey)
+    client = tmt_client.TmtClient(cred, "ap-beijing")
+
+    req = models.TextTranslateRequest()
+    req.SourceText = phrase
+    req.Source = source
+    req.Target = target
+    req.ProjectId = 0
+
+    resp = client.TextTranslate(req)
+    data = json.loads(resp.to_json_string())
+    result = data["TargetText"]
+
+    await ctx.send(result)
 
 
-
+bot.run(token)
